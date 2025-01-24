9cb83696bccc1ec6413c9ba82b6f8e60d1ed8cdf
Alex <alexjstewart220@gmail.com>
2025-01-24 12:51:51 -0500

    changes h1 for testing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App(){
     return (
         <div className="app">
-            <h1>This simple, full-stack site is designed to help you understand
+            <h1>This is an AWESOME, full-stack site is designed to help you understand
                 continuous integration and continuous deployment!
             </h1>
         </div>
